020b3f962a4764dcf423c0692b7823d1f0beaeb2
SnehashishMishra <snehashishmishra18@gmail.com>
2026-06-29 01:14:37 +0530

    Remove LiquidButton component and update Hero layout

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,6 @@ import { TypeAnimation } from "react-type-animation";
 
 import { useTheme } from "@/components/theme-provider";
 
-import { LiquidButton } from "./button/LiquidButton";
 import Particles from "./Particles";
 
 export default function Hero() {
@@ -126,24 +125,9 @@ export default function Hero() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.4 }}
-          className="mt-12 mb-7 flex flex-col justify-center gap-12 sm:mb-12 sm:flex-row"
+          className="mt-12 mb-7 flex flex-col justify-center gap-12 p-10 sm:mb-12 sm:flex-row"
         >
-          <div
-            style={{
-              boxShadow: "0 0 20px rgba(6, 182, 212, 0.4)",
-              borderRadius: "100%",
-            }}
-          >
-            <LiquidButton
-              color="from-primary to-secondary cursor-pointer bg-linear-to-r"
-              textColor="text-primary-foreground"
-              buttonClassName="px-6 py-2 font-semibold transition-all rounded-full sm:px-8 rounded-full sm:py-3 "
-              onClick={scrollToProjects}
-            >
-              Explore My Work
-            </LiquidButton>
-          </div>
-          {/* <motion.button
+          <motion.button
             onClick={scrollToProjects}
             whileHover={{
               scale: 1.03,
@@ -154,7 +138,7 @@ export default function Hero() {
             className="from-primary to-secondary text-primary-foreground cursor-pointer rounded-full bg-linear-to-r px-6 py-2 font-semibold transition-all hover:shadow-lg sm:px-8 sm:py-3"
           >
             Explore My Work
-          </motion.button> */}
+          </motion.button>
 
           <motion.div
             variants={itemVariants}
